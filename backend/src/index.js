e99dede1ff15569edceaf628aecca5e743dfd06a
Aleksandar Ristic <storiaca@gmail.com>
2018-12-07 14:03:47 +0100

    Currently logged in user middlware backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
@@ -7,8 +8,18 @@ const db = require('./db');
 const server = createServer();
 
 // Use express middleware to handle cookies (JWT)
-server.express.use(cookieParser())
-// TODO Use express middleware to populate current user
+server.express.use(cookieParser());
+
+// Decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if(token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // Put the userId onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
