7a3584f262551ca109d5e0c25e81f3492c092035
storiaca <storiaca@gmail.com>
2018-12-08 13:21:10 +0100

    Sign in form

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -30,11 +30,11 @@ const Mutations = {
       },
       info
     );
-  }, 
+  },
   async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id };
     // 1. Find the item
-    const item = await ctx.db.query.item({ where }, `{ id title }`)
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
     // 2. Check if they own that item, or have the permissions
     // TODO
     // 3. Delete it!
@@ -51,19 +51,40 @@ const Mutations = {
         data: {
           ...args,
           password,
-          permissions: { set: ['USER'] }
+          permissions: { set: ["USER"] }
         }
-      }, 
+      },
       info
     );
     // Create the JWT token for them
-    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
     // We set the jwt as a cookie on the response
-    ctx.response.cookie('token', token, {
+    ctx.response.cookie("token", token, {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 60 * 24 * 365, // 1 year cookie
-    })
+      maxAge: 1000 * 60 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
     // Finaly we return the user to the browser
+    return user;
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    // 1. Check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email: email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    // 2. Check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error("Invalid Password");
+    }
+    // 3. Generate the JWT Token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // 4. Set the cookie with the token
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
+    // 5. Return the user
     return user;
   }
 };
