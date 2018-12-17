f44bc976e0271f9052287580df04870956db541b
Aleksandar Ristic <storiaca@gmail.com>
2018-12-17 16:15:19 +0100

    Server side add to cart backend

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -201,6 +201,37 @@ const Mutations = {
         id: args.userId
       },
     }, info);
+  },
+  async addToCart(parent, args, ctx, info) {
+    // 1. Make sure they are signed in
+    const { userId } = ctx.request;
+    if(!userId) {
+      throw new Error('You must be signed in!');
+    }
+    // 2. Query the users current cart
+    const [existingCartItem] = ctx.db.query.cartItems({
+      user: { id: userId },
+      item: { id: args.id }
+    });
+    // 3. Check if that item is already in their cart and increment by 1 if it is
+    if (existingCartItem) {
+      console.log('This item is already in their cart');
+      return ctx.db.mutation.updateCartItem({
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + 1 }
+      });
+    }
+    // 4. If its not, create a fresh CartItem for that user!
+    return ctx.db.mutation.createCartitem({
+      data: {
+        user: {
+          connect: { id: userId },
+        },
+        item: { 
+          connect: { id: args.id }
+        }
+      }
+    })
   }
 };
 
