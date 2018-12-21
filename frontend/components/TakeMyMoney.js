54e6cc97fe503fef3a24fe1d29a3271aadedf4a1
Aleksandar Ristic <storiaca@gmail.com>
2018-12-21 16:56:18 +0100

    Implement StripeCheckout

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -9,9 +9,33 @@ import calcTotalPrice from "../lib/calcTotalPrice";
 import Error from "./ErrorMessage";
 import User, { CURREN_USER_QUERY } from "./User";
 
+function totalItems(cart) {
+  return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
+}
 class TakeMyMoney extends Component {
+  onToken = res => {
+    console.log("On Token Called");
+    console.log(res);
+  };
   render() {
-    return <User>{({ data: { me } }) => <p>{this.props.children}</p>}</User>;
+    return (
+      <User>
+        {({ data: { me } }) => (
+          <StripeCheckout
+            amount={calcTotalPrice(me.cart)}
+            name="Sick Fits"
+            description={`Order of ${totalItems(me.cart)} items`}
+            image={me.cart[0].item && me.cart[0].item.image}
+            stripeKey="pk_test_EN7BxxEzLkj7hxzIPpRKyUDh"
+            currency="USD"
+            email={me.email}
+            token={res => this.onToken(res)}
+          >
+            {this.props.children}
+          </StripeCheckout>
+        )}
+      </User>
+    );
   }
 }
 
