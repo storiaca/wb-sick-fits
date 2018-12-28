a16f6ce1744a0918669ca29aa6dd5b9fa9b9ddf5
Aleksandar Ristic <storiaca@gmail.com>
2018-12-28 15:35:59 +0100

    Testing with it and expect examples

--- frontend/__tests__/sample.test.js
+++ frontend/__tests__/sample.test.js
@@ -1,3 +1,18 @@
 describe("sample test 101", () => {
-  console.log("I am running some tests!~");
+  it("works as expected", () => {
+    const age = 100;
+    expect(1).toEqual(1);
+    expect(age).toEqual(100);
+  });
+
+  it("handles ranges just fine", () => {
+    const age = 200;
+    expect(age).toBeGreaterThan(100);
+  });
+
+  fit("makes a list of dog names", () => {
+    const dogs = ["snickers", "hugo"];
+    expect(dogs).toEqual(dogs);
+    expect(dogs).toContain("snickers");
+  });
 });
