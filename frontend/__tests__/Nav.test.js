5de993a4afa21c604841f0ebeeb740186eeb573a
Aleksandar Ristic <storiaca@gmail.com>
2019-01-18 13:44:28 +0100

    Test Nav component start

--- frontend/__tests__/Nav.test.js
+++ frontend/__tests__/Nav.test.js
@@ -4,7 +4,7 @@ import toJSON from "enzyme-to-json";
 import Nav from "../components/Nav";
 import { CURRENT_USER_QUERY } from "../components/User";
 import { MockedProvider } from "react-apollo/test-utils";
-import { fakeUser } from "../lib/testUtils";
+import { fakeUser, fakeCartItem } from "../lib/testUtils";
 
 const notSignedInMocks = [
   {
@@ -20,6 +20,20 @@ const signedInMocks = [
   }
 ];
 
+const signedInMocksWithCartItems = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: {
+      data: {
+        me: {
+          ...fakeUser(),
+          cart: [fakeCartItem(), fakeCartItem(), fakeCartItem()]
+        }
+      }
+    }
+  }
+];
+
 describe("<Nav/>", () => {
   it("renders a minimal nav when signed out", async () => {
     const wrapper = mount(
@@ -30,7 +44,7 @@ describe("<Nav/>", () => {
     await wait();
     wrapper.update();
     //console.log(wrapper.debug());
-    const nav = wrapper.find('[data-test="nav"]');
+    const nav = wrapper.find('ul[data-test="nav"]');
     expect(toJSON(nav)).toMatchSnapshot();
   });
 
@@ -40,5 +54,23 @@ describe("<Nav/>", () => {
         <Nav />
       </MockedProvider>
     );
+    await wait();
+    wrapper.update();
+    const nav = wrapper.find('ul[data-test="nav"]');
+    expect(nav.children().length).toBe(6);
+    expect(nav.text()).toContain("Sign Out");
+  });
+
+  it("renders the amount of items in the cart", async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocksWithCartItems}>
+        <Nav />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    const nav = wrapper.find('[data-test="nav"]');
+    const count = nav.find("div.count");
+    expect(toJSON(count)).toMatchSnapshot();
   });
 });
